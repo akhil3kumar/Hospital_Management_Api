e75dfcf7d259743b0551d69be6a9fbb8a5b6a02a
akhil3kumar <akhil.03.kumar@gmail.com>
2026-06-24 15:38:46 +0530

    new commit

---
+++ billing-service/src/test/java/com/akhil/billing_service/BillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akhil.billing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
